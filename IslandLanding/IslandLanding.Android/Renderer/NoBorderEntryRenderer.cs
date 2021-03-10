ccbe84d6123b808f712b38577ef4ce1b27407220
amira <amiraelsayed62@hotmail.com>
2021-03-10 23:04:37 +0200

    GamerTag Ui
    Create the gamer tag ui as figma design

---
+++ IslandLanding/IslandLanding.Android/Renderer/NoBorderEntryRenderer.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using IslandLanding.Controls;
+using IslandLanding.Droid.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+[assembly: ExportRenderer(typeof(NoBorderEntry), typeof(NoBorderEntryRenderer))]
+namespace IslandLanding.Droid.Renderer
+{
+ public class NoBorderEntryRenderer: EntryRenderer
+  {
+    public NoBorderEntryRenderer(Context context) : base(context)
+    {
+    }
+
+    protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+    {
+      base.OnElementChanged(e);
+
+      if (this.Control != null)
+      {
+        var roundedEntry = Element as NoBorderEntry;
+        if (roundedEntry != null)
+        {
+          GradientDrawable gd = new GradientDrawable();
+          gd.SetColor(global::Android.Graphics.Color.Transparent);
+          this.Control.SetBackgroundDrawable(gd);
+          this.Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+          Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.White));
+
+        }
+
+       
+
+       
+      }
+    }
+
+
+  }
+}
