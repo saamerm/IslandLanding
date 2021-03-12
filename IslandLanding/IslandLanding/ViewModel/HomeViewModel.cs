8fb1dbe7b2d3514bf4f8ad0a788553adbe4c8138
amira <amiraelsayed62@hotmail.com>
2021-03-12 05:35:13 +0200

    Navigation
    when click on laederboard naviagte to leaderboardpage
    
    when click on start navigate to DifficulltyPage
    
    when click on Infoicon naviagte  to infoPage

--- IslandLanding/IslandLanding/ViewModel/HomeViewModel.cs
+++ IslandLanding/IslandLanding/ViewModel/HomeViewModel.cs
@@ -10,10 +10,32 @@ namespace IslandLanding.ViewModel
  public class HomeViewModel:BaseViewModel
   {
     public ICommand ProfileCommand { get; set; }
+    public ICommand LeaderBoardCommand { get; set; }
+    public ICommand StartCommand { get; set; }
+    public ICommand InfoCommand { get; set; }
     public HomeViewModel()
     {
       ProfileCommand = new Command(ProfileCommandExcute);
+      LeaderBoardCommand = new Command(LeaderBoardCommandExcute);
+      StartCommand = new Command(StartCommandExcute);
+      InfoCommand = new Command(InfoCommandExcute);
     }
+
+    private void InfoCommandExcute(object obj)
+    {
+      App.Current.MainPage.Navigation.PushAsync(new InfoPage());
+    }
+
+    private void StartCommandExcute(object obj)
+    {
+      App.Current.MainPage.Navigation.PushAsync(new DificullityPage());
+    }
+
+    private void LeaderBoardCommandExcute(object obj)
+    {
+      App.Current.MainPage.Navigation.PushAsync(new LeaderBoardPage());
+    }
+
     private void ProfileCommandExcute(object obj)
     {
       App.Current.MainPage.Navigation.PushAsync(new SwitchUserAccountPage());
