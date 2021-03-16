6c9c4fb70e957567652f3685a256a9aadce9a996
amira <amiraelsayed62@hotmail.com>
2021-03-16 15:58:31 +0200

    Add click on check us
    when press on check us will navigate to website

--- IslandLanding/IslandLanding/ViewModel/InfoViewModel.cs
+++ IslandLanding/IslandLanding/ViewModel/InfoViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace IslandLanding.ViewModel
@@ -15,8 +16,10 @@ namespace IslandLanding.ViewModel
       set => SetProperty(ref _rules, value);
     }
     public ICommand BackCommand { get; set; }
+    public ICommand CheckusCommand { get; set; }
     public InfoViewModel()
     {
+      CheckusCommand = new Command(CheckusCommandExcute);
       BackCommand = new Command(BackCommandExcute);
       Rules = @"Use this brain training game to improve your time sensibitiliy. When you are ready, you can click the Ready button and count the assigned time by the captain in your mind. 
 
@@ -24,6 +27,19 @@ You can click on the Launch Parachute button when you think the time has elapsed
 
 This game is developed by The First Prototype.We help mers love, and we specialize in mobile app development.Background vectors created by pikisuperstar";
     }
+
+    private async void CheckusCommandExcute(object obj)
+    {
+      try
+      {
+        await Browser.OpenAsync("https://thefirstprototype.com/", BrowserLaunchMode.SystemPreferred);
+      }
+      catch (Exception ex)
+      {
+        // An unexpected error occured. No browser may be installed on the device.
+      }
+    }
+
     private void BackCommandExcute(object obj)
     {
       App.Current.MainPage.Navigation.PopAsync();
