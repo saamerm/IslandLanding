2da5adb34463652bf591d272c04466a5d98abb73
amira <amiraelsayed62@hotmail.com>
2021-03-16 16:32:51 +0200

    DifficuiltyPage
    create design for difficulty page
    back button function worked

---
+++ IslandLanding/IslandLanding/ViewModel/DifficuilityViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IslandLanding.ViewModel
+{
+  public class DifficuilityViewModel : BaseViewModel
+  {
+    public ICommand BackCommand { get; set; }
+    public DifficuilityViewModel()
+    {
+      BackCommand = new Command(BackCommandExcute);
+    }
+
+    private void BackCommandExcute(object obj)
+    {
+      App.Current.MainPage.Navigation.PopAsync();
+    }
+  }
+}
