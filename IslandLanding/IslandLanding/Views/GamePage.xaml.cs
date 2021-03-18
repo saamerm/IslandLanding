8f4093b4a72e2e78e72eda370563abc45a0a4c36
amira <amiraelsayed62@hotmail.com>
2021-03-18 23:03:08 +0200

    Design start game
    play animation and start timer

--- IslandLanding/IslandLanding/Views/GamePage.xaml.cs
+++ IslandLanding/IslandLanding/Views/GamePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IslandLanding.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace IslandLanding.Views
     public GamePage()
     {
       InitializeComponent();
+      BindingContext = new GameViewModel();
     }
   }
 }
